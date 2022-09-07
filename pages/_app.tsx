2b36b11a295a60490f5a19e1bef9c7df6514cc82
Mijail Piekarz <piekarzmijail@gmail.com>
2022-09-07 11:29:10 -0300

    Removed duplicated Head on _app.tesx and index.tsx. Moved the custom fonts to the new _document.tsx page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import type { AppProps } from "next/app";
-import { Head } from "next/document";
 import { Navbar } from "../components/navbar/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  <Head>
-    <title>Avobankless</title>
-    <meta
-      name="description"
-      content="The decentralized credit bureau for the unbanked inclusive, secure and private."
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charSet="utf-8" />
-    <meta name="theme-color" content="#000000" />
-  </Head>;
   return (
     <>
       <div className="h-screen bg-body">
